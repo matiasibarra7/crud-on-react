4003b435d0d158e7c0039b8ad64d80d7e29437de
Matias <matiasibarra7@gmail.com>
2021-03-08 19:41:17 -0300

    First Commit

---
+++ src/NameList.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+function NameList() {
+ 
+
+  return (
+    <>
+      <h2>Aplicación basica de CRUD en REACT</h2>
+
+    </>
+
+  );
+}
+
+export default NameList;
